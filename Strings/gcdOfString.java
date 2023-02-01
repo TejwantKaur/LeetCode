a941a020f26f3a30fc40b0367108b334d8a42844
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2023-02-01 23:51:06 +0530

    Add files via upload

---
+++ Strings/gcdOfString.java
@@ -0,0 +1,19 @@
+public class gcdOfString{
+    public static String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        return str1.substring(0,gcd(str1.length(),str2.length()));
+    }
+    public static int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    public static void main(String args[]){
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        System.out.println(gcdOfStrings(str1,str2)); 
+    }
+}
