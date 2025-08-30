ba35264e23f436f8f85626c8e4ec98969eebad47
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-08-30 23:27:35 +0530

    Time: 13 ms (20.67%), Space: 45.1 MB (16.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,61 +1,17 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return helper(board,0,0);
-    }
-
-    public boolean helper(char[][] board, int row, int col){
-        if(row==board.length){
-            return true;
-        }
-        int nrow = 0;
-        int ncol = 0; 
-        if(col != board.length-1){
-            nrow = row;
-            ncol = col+1;
-        }
-        else{
-            nrow = row+1;
-            ncol = 0;
-        }
-        if(board[row][col] != '.'){
-            if(helper(board,nrow,ncol)){
-                return true;
-            }
-        }
-        else{
-            for(int digit = 1; digit<=9; digit++){
-                if(isSafe(board,row,col,digit)){
-                    board[row][col] = (char)(digit+'0');
-                    if(helper(board,nrow,ncol)){
-                        return true;
-                    }
-                    else{
-                        board[row][col] = '.';
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean isSafe(char[][] board, int row, int col, int digit){
+        HashSet<String> hs = new HashSet<>();
 
         for(int i=0; i<board.length; i++){
-            if(board[i][col] == (char)(digit+'0')){
-                return false;
-            }
-            if(board[row][i] == (char)(digit+'0')){
-                return false;
-            }
-        }
-        int sr = (row/3)*3;
-        int sc = (col/3)*3;
-
-        for(int i=sr; i<sr+3; i++){
-            for(int j=sc; j<sc+3; j++){
-                if(board[i][j] == (char)(digit+'0')){
-                    return false;
-                }
+            for(int j=0; j<board.length; j++){
+                char num = board[i][j];
+                if (num!='.'){
+                    if (!hs.add(num + " in row " + i) ||
+                        !hs.add(num + " in col " + j) ||
+                        !hs.add(num + " in box " + (i / 3) + "-" + (j / 3))) {
+                        return false;
+                    }
+                } 
             }
         }
         return true;
