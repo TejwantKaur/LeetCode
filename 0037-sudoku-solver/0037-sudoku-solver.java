259ad563be817f455a64f6e8b540cfb3e4e6cc02
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-08-31 10:10:22 +0530

    Time: 191 ms (72%), Space: 41.1 MB (71.9%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,132 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+
+    public boolean solve(char[][] board, int row, int col){
+        // Base case
+        if (row==9) return true;
+
+        // recursion
+        int nextRow=row, nextCol=col+1;
+        if (nextCol==9){
+            nextRow = row+1;
+            nextCol = 0;
+        }
+
+        if (board[row][col] != '.'){
+            return solve(board, nextRow, nextCol);
+        }
+
+        // for "." Place Digit
+        for(int dig=1; dig<=9; dig++){
+            char ch = (char) (dig +'0');
+            if (isSafe (board, row, col, ch)){
+                board[row][col] = ch;
+                if(solve(board, nextRow, nextCol)){
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        } 
+        return false;
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, char dig){
+        
+        // colm
+        for (int i=0; i<9; i++){
+            if (board[i][col] == dig){
+                return false;
+            }
+        }
+        // row
+        for (int j=0; j<9; j++){
+            if (board[row][j] == dig){
+                return false;
+            }
+        }
+        // grid
+        int sr = (row/3)*3; //starting row
+        int sc = (col/3)*3;
+
+        for(int i=sr; i<sr+3; i++){
+            for (int j=sc; j<sc+3; j++){
+                if (board[i][j] == dig){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // public boolean solve(char[][] board, int row, int col)
+    // {
+    //     if(col == board[0].length){
+    //         col = 0;
+    //         row++;
+    //     }
+
+    //     if(row == board.length){
+    //         return true;
+    //     }
+        
+    //     if(board[row][col] != '.'){
+    //         return solve(board, row, col + 1);
+    //     }
+            
+        
+    //     for(int i = 1; i <= 9; i++){
+    //         char c = (char)(i + '0');
+            
+    //         if(isValidMove(board, row, col, c)){
+    //             board[row][col] = c;
+                
+    //             if(solve(board, row, col + 1)){
+    //                 return true;
+    //             }        
+    //         }
+            
+    //         board[row][col] = '.';
+    //     }
+    //     return false;
+    // }
+    
+    // public boolean isValidMove(char[][] board, int row, int col, char ch)
+    // {
+    //     for(int i=0; i<board.length; i++){
+    //         if(board[i][col] == ch || board[row][i] == ch){
+    //             return false;
+    //         }
+    //     }
+    //     int sr = (row/3)*3;
+    //     int sc = (col/3)*3;
+
+    //     for(int i=sr; i<sr+3; i++){
+    //         for(int j=sc; j<sc+3; j++){
+    //             if(board[i][j] == ch){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+}
