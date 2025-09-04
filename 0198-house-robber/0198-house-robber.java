d1e9a3755ce98b1ff5415f3052dad39e789b7d3a
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-09-04 23:54:53 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -17,25 +17,4 @@ class Solution {
         return prev1;
     }
 
-    // public int rob(int[] nums) {
-    //     int n = nums.length;
-    //     int dp[] = new int[n];
-    //     Arrays.fill(dp,-1);
-    //     int idx = 0;
-    //     return helper(nums, idx, dp);
-    // }
-
-    // public int helper(int[]nums, int idx, int[]dp){
-    //     if (idx>=nums.length) return 0;
-
-    //     if (dp[idx] != -1) return dp[idx];
-
-    //     int rob = nums[idx] + helper(nums, idx+2, dp);
-    //     int skip = helper(nums, idx+1, dp);
-
-    //     dp[idx] = Math.max(rob, skip);
-    //     return dp[idx];
-    // }
-
-
 }
