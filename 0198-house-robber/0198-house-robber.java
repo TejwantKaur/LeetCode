2f6163caf605f76f628ffbeed69c44ad79fc65fe
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-09-04 23:51:59 +0530

    Time: 0 ms (100%), Space: 40.8 MB (89.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,43 +1,4 @@
 class Solution {
-    // public int rob(int[] nums) {
-    //     int n = nums.length;
-
-    //     if (n==0) return 0;
-    //     if (n==1) return nums[0];
-
-    //     int prev2 = nums[0];
-    //     int prev1 = Math.max(nums[0], nums[1]);
-
-    //     for(int i=2; i<n; i++){
-    //         int curr = Math.max(nums[i]+prev2, prev1);
-
-    //         prev2 = prev1;
-    //         prev1 = curr;
-    //     }
-    //     return prev1;
-    // }
-
-    // public int rob(int[] nums) {
-    //     int n = nums.length;
-    //     int dp[] = new int[n];
-    //     Arrays.fill(dp,-1);
-    //     int idx = 0;
-    //     return helper(nums, idx, dp);
-    // }
-
-    // public int helper(int[]nums, int idx, int[]dp){
-    //     if (idx>=nums.length) return 0;
-
-    //     if (dp[idx] != -1) return dp[idx];
-
-    //     int rob = nums[idx] + helper(nums, idx+2, dp);
-    //     int skip = helper(nums, idx+1, dp);
-
-    //     dp[idx] = Math.max(rob, skip);
-    //     return dp[idx];
-    // }
-
-
     public int rob(int[] nums) {
         int n=nums.length;
 
