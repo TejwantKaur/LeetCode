b1585ce38c7a2699b74083806b0dcb9bd3c6e369
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-09-15 23:39:57 +0530

    Time: 3 ms (49.69%), Space: 42.7 MB (31.76%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -1,34 +1,23 @@
 class Solution {
+    private boolean isVowel(char c){
+        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    }
     public int maxFreqSum(String s) {
         HashMap<Character, Integer> hm = new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if (hm.containsKey(c)) {
-                hm.put(c, hm.get(c)+1);
-            }else{
-                hm.put(c,1);
-            }
-        }
-        
-        char[] vowels = {'a','e','i','o','u'}; 
-        char[] cons = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
-
-        int maxv=0;
-        for(char c:vowels){
-            if (hm.containsKey(c)){
-                maxv = Integer.max(hm.get(c), maxv);
-
-            }
+            hm.put(c, hm.getOrDefault(c,0)+1);
         }
 
-        int maxc=0;
-        for(char c:cons){
-            if (hm.containsKey(c)){
-                maxc = Integer.max(hm.get(c), maxc);
-            }
+        int maxv = 0, maxc=0;
+        for(char c: hm.keySet()){
+            int freq = hm.get(c);
+            if(isVowel(c)) 
+                maxv = Math.max(freq, maxv);
+            else
+                maxc = Math.max(freq,maxc);   
         }
-        int out = maxc+maxv;
-        return out;
+        return maxc+maxv;
     }
 }
