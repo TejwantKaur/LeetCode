98d004d0573dba4019bfa524081445b7a273cec5
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-09-22 10:43:05 +0530

    Time: 1 ms (44.35%), Space: 45 MB (69.72%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean[][] vis = new boolean[image.length][image[0].length];
+        helper(image,sr,sc,color,vis,image[sr][sc]);
+        return image;
+    }
+    public void helper(int[][] image, int sr, int sc, int color, boolean[][]vis, int stClr){ // starting color to be modified
+        if(sr<0||sc<0 || sr>=image.length||sc>=image[0].length || vis[sr][sc] || image[sr][sc]!=stClr)
+            return;
+
+        vis[sr][sc] = true;
+        image[sr][sc] = color;
+
+        // left
+        helper(image, sr, sc-1, color, vis, stClr);
+
+    // right
+        helper(image, sr, sc+1, color, vis, stClr);
+
+    // up
+        helper(image, sr-1, sc, color, vis, stClr);
+
+    // down
+        helper(image, sr+1, sc, color, vis, stClr);
+}
+}
