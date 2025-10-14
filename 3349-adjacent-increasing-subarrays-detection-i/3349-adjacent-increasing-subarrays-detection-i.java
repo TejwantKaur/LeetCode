8e82dbfb3d758d4ee5db7355ffd758ee5c14dc82
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-10-15 01:20:41 +0530

    Time: 2 ms (48.53%), Space: 44.5 MB (49.26%) - LeetHub

---
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n=nums.size();
+        if(n<2*k) return false;
+       
+        int streak[] = new int[n]; //length; of inc subsequence eding at i
+        streak[0] = 1;
+
+        // calculate streaks
+        for(int i=1; i<n; i++){
+            if(nums.get(i) > nums.get(i-1))
+                streak[i] = streak[i-1]+1;
+            else
+                streak[i] = 1;
+        }
+
+        // chk for 2 adjacent
+        for(int i=k-1; i+k<n; i++){ //i= 3-1=2, 2+3<n
+            if(streak[i]>=k && streak[i+k]>=k)
+                return true;
+        }
+        return false;
+    }
+}
