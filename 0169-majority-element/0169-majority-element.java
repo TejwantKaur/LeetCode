a18982697dcc073db6c1253096b15147d2897ce4
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-10-24 13:39:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        if(n==1) return 1;
+
+        Arrays.sort(nums);
+        int freq=1; 
+        int ans=nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]==nums[i-1]){
+                freq++;
+            }
+            else{
+                freq=1; ans=nums[i];
+            }
+            if (freq>n/2) 
+                return ans;
+        }
+        return -1;
+    }
+}
