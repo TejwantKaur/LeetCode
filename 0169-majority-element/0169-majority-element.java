0b082f9e99df52706af287c57f64d29b26841fc2
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-10-24 13:49:04 +0530

    Time: 1 ms (99.73%), Space: 53.4 MB (31.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,19 +3,27 @@ class Solution {
         int n=nums.length;
         if(n==1) return nums[0];
 
-        Arrays.sort(nums);
-        int freq=1; 
-        int ans=nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1]){
-                freq++;
-            }
-            else{
-                freq=1; ans=nums[i];
-            }
-            if (freq>n/2) 
-                return ans;
+        int freq=0; int ans=0;
+        for(int i=0; i<n; i++){
+            if(freq==0)
+                ans=nums[i];
+            if (nums[i]==ans) freq++;
+            else freq--;
         }
-        return -1;
+        return ans;
+        // Arrays.sort(nums);
+        // int freq=1; 
+        // int ans=nums[0];
+        // for(int i=1; i<n; i++){
+        //     if(nums[i]==nums[i-1])
+        //         freq++;
+        //     else{
+        //         freq=1; 
+        //         ans=nums[i];
+        //     }
+        //     if (freq>n/2) 
+        //         return ans;
+        // }
+        // return -1;
     }
 }
