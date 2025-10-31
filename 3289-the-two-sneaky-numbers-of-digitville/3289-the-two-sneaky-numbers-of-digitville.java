5349b63631b6f980d983a09a36621ef41d19de06
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2025-10-31 23:16:49 +0530

    Time: 2 ms (63.09%), Space: 46.5 MB (7.71%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,14 +1,30 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int[] ans = new int[2];
         int n = nums.length;
-        Arrays.sort(nums);
-
+        HashSet<Integer> hs = new HashSet<>();
+        int[] ans = new int[2];
         int j=0;
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1])
+        for(int i=0; i<n; i++){
+            if(hs.contains(nums[i])){
                 ans[j++]=nums[i];
+            } 
+            else {
+                hs.add(nums[i]);
+            }
         }
         return ans;
+        
     }
 }
+
+
+       
+        
+        // Arrays.sort(nums);
+
+        // int j=0;
+        // for(int i=1; i<n; i++){
+        //     if(nums[i]==nums[i-1])
+        //         ans[j++]=nums[i];
+        // }
+        // return ans;
