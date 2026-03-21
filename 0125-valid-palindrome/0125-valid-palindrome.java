a57972905f9448559cf7e82832edb32ab505f7ed
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-03-21 13:00:02 +0530

    Time: 3 ms (61.29%), Space: 44.2 MB (82.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,31 +1,20 @@
 class Solution {
-    private boolean alphaNum(char ch){
-        ch = Character.toLowerCase(ch);
-        if((ch >= '0' && ch <= '9') ||
-        (ch >= 'a' && ch <= 'z')){
-            return true;
-        }
-        return false;
-    }
     public boolean isPalindrome(String s) {
-        int st = 0, end = s.length()-1;
+      int n=s.length();
+      s = s.toLowerCase();
+    
+      int i=0, j=n-1;
+      while(i<j){
+        while(!Character.isLetterOrDigit(s.charAt(i)) && i<j) i++;
+        while(!Character.isLetterOrDigit(s.charAt(j)) && i<j) j--;
 
-        while(st < end) {
-            if(!alphaNum(s.charAt(st))){
-                st++; continue;
-            }
-            if(!alphaNum(s.charAt(end))){
-                end--; continue;
-            }
-            
-            char ch1 = Character.toLowerCase(s.charAt(st));
-            char ch2 = Character.toLowerCase(s.charAt(end));
+        char ch1=s.charAt(i);
+        char ch2=s.charAt(j);
 
-            if(ch1 != ch2)
-                return false;
-
-            st++; end--;
-        }
-        return true;
+        if(ch1!=ch2) return false;
+        
+        i++; j--;
+      }
+      return true;
     }
 }
