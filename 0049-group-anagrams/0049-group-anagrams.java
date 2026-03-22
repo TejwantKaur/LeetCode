b87bf320bf5defb61ecc72f6decc55e719f5647a
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-03-22 21:11:50 +0530

    Time: 19 ms (19.11%), Space: 51 MB (8.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,40 +1,18 @@
 class Solution {
-  public static String string_compression(String s){
-    // HashMap<Character,Integer> hm = new HashMap<>();
-    // for(char c: s.toCharArray()){
-    //   hm.put(c,hm.getOrDefault(c,0)+1);
-    // }
-    int freq[] = new int[26];
-    for(char c: s.toCharArray()){
-      freq[c-'a']++;
-    }
-
-    // StringBuilder sb = new StringBuilder();
-    // char c='a';
-    // for(int i:freq){
-    //   sb.append(c);
-    //   sb.append(i);
-    // }
-    // return sb.toString();
-    return Arrays.toString(freq);
-    // return ret;
-  }
-
   public List<List<String>> groupAnagrams(String[] strs) {
     HashMap<String, List<String>> map = new HashMap<>();
-    for (String str:strs){
-      String strComp = string_compression(str);
-      // compressed string, original
-      // if(map.containsKey(strComp)) {
-      //   map.get(strComp).add(str);
-      // }
-      // else {
-      //   List<String> list = new ArrayList<>();
-      //   list.add(str);
-      //   map.put(strComp,list);
-      // }
-      map.computeIfAbsent(strComp, k -> new ArrayList<>()).add(str);
+
+        for (String str : strs) {
+            int[] count = new int[26];
+
+            for (char ch : str.toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            String substr = Arrays.toString(count);
+            map.computeIfAbsent(substr, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
     }
-    return new ArrayList<>(map.values());      
-  }
 }
