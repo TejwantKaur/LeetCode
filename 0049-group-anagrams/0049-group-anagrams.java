3f2d17ed950414278a3e34c509b8be0c851fe178
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-03-22 21:02:29 +0530

    Time: 18 ms (24.51%), Space: 51.1 MB (7.18%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,31 @@
+class Solution {
+  public static String string_compression(String s){
+    // HashMap<Character,Integer> hm = new HashMap<>();
+    // for(char c: s.toCharArray()){
+    //   hm.put(c,hm.getOrDefault(c,0)+1);
+    // }
+    int fre[] = new int[26];
+    for(char c: s.toCharArray()){
+      fre[c-'a']++;
+    }
+    String ret = Arrays.toString(fre);
+    return ret;
+  }
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> map = new HashMap<>();
+    for (String str:strs){
+      String strComp = string_compression(str);
+      // compressed string, original
+      if(map.containsKey(strComp)) {
+        map.get(strComp).add(str);
+      }
+      else {
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        map.put(strComp,list);
+      }
+    }
+    return new ArrayList<>(map.values());      
+  }
+}
