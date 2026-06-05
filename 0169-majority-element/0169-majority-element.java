e022adadf24b654bc014f19a1564f30293f991cb
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-06-05 16:52:15 +0530

    Time: 9 ms (36.8%), Space: 55.4 MB (69.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,29 +1,20 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
+    public int majorityElement(int[] nums){
+      int n=nums.length;
+      if(n==0 || n==1) return nums[0];
 
-        int freq=0; int ans=0;
-        for(int i=0; i<n; i++){
-            if(freq==0)
-                ans=nums[i];
-            if (nums[i]==ans) freq++;
-            else freq--;
-        }
-        return ans;
-        // Arrays.sort(nums);
-        // int freq=1; 
-        // int ans=nums[0];
-        // for(int i=1; i<n; i++){
-        //     if(nums[i]==nums[i-1])
-        //         freq++;
-        //     else{
-        //         freq=1; 
-        //         ans=nums[i];
-        //     }
-        //     if (freq>n/2) 
-        //         return ans;
-        // }
-        // return -1;
+      Arrays.sort(nums);
+      int freq=1;
+
+      for(int i=1; i<n; i++){
+        if(nums[i]==nums[i-1]) {
+            freq++;
+            if(freq>n/2) {
+              return nums[i];
+            };
+        } else freq=1; 
+      }
+      return 0;
+        
     }
 }
