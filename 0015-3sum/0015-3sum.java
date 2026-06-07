fddf3a76645bcb27883d5beb728c010973abab46
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-06-07 23:54:34 +0530

    Time: 34 ms (59.96%), Space: 59.2 MB (58.8%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,25 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
+  public List<List<Integer>> threeSum(int[] nums) {
+    Arrays.sort(nums);
 
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
+    List<List<Integer>> list = new ArrayList<>();
+    for(int i=0; i<nums.length-2; i++){
+      if(i>0 && nums[i]==nums[i-1]) continue;
 
-            int j=i+1, k=n-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0)
-                    j++;
-                else if(sum>0)
-                    k--;
-                else{
-                    // sum==0
-                    List<Integer> ls = new ArrayList<>(); 
-                    ls.add(nums[i]);
-                    ls.add(nums[j]);
-                    ls.add(nums[k]);
-                    ans.add(ls);
-                    j++; k--;
+      int fix = nums[i];
+      int l=i+1, r=nums.length-1;
+      while(l<r){
+        if(nums[l] + nums[r] == -fix){
+          list.add(List.of(fix,nums[l],nums[r]));
+          while(l<r && nums[l]==nums[l+1]) l++;
+          while(r>l && nums[r]==nums[r-1]) r--;
 
-                    while(j<k && nums[j]==nums[j-1])
-                        j++;
-                }
-            }
+          l++; r--;
         }
-        return ans;
-    }
+        else if(nums[l] + nums[r] > -fix) r--;
+        else l++;
+      }
+    }  
+    return list;
+  }
 }
