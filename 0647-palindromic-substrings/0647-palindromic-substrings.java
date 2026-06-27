49f581e839d77453afa1e72ef49e9964cd38de60
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-06-27 15:01:06 +0530

    Time: 1141 ms (5.03%), Space: 52.6 MB (8.16%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -5,10 +5,24 @@ class Solution {
 
       return rec(s, ++i, --j); 
     }
+
+    public boolean memo(String s, int i, int j, int dp[][]){
+      if(i>=j) return true;
+      if(dp[i][j] != 0) 
+        return dp[i][j] == 1;
+        // return dp[i][j] == 1? true: if -1 false;
+
+      dp[i][j] = memo(s, ++i, --j, dp)? 1: -1;
+      return dp[i][j] == 1;
+    }
+
     public int countSubstrings(String s) {
+      int n=s.length();
       int cnt=0;
-      for(int i=0; i<s.length(); i++){
-        for(int j=i; j<s.length(); j++){
+
+      int[][]dp = new int[n+1][n+1];
+      for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
           if (rec(s, i, j)) cnt++;
         }
       }
