827f36380deef0268ef8a83b1fc3d0c098f8bcd5
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-06-28 12:24:06 +0530

    Time: 2 ms (8.07%), Space: 45.1 MB (5.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,31 @@
 class Solution {
+    static void swap(int[]nums, int i, int j){
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+    }
+
+    static void perm(int[] nums){
+      int n=nums.length;
+
+      for(int i=n-1; i>0; i--){
+        if(nums[i]>nums[i-1]){
+          int greatest = n-1;
+          while(nums[greatest] <= nums[i-1])
+            greatest--;
+
+          swap(nums, i-1, greatest);
+          Arrays.sort(nums,i,n);
+          return;
+        }
+      }
+      // if descending
+      // nums = [3,2,1];
+      Arrays.sort(nums);
+      return;
+    }
+
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int i=n-2;
-
-        // 1) find 1st decreasing from end
-        while(i>=0 && nums[i]>=nums[i+1]) 
-            i--;
-        
-        // 2) next greater elem from right
-        if(i>=0){
-            int j=n-1;
-            while(nums[j]<=nums[i])
-                j--;
-            swap(nums, i, j);
-        }
-        // 3) reverse (i+1, n-1);
-        reverse(nums,i+1,n-1);
-    }
-
-    public void swap(int nums[], int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
-
-    public void reverse(int nums[], int i, int j){
-        while(i<=j){
-            swap(nums,i,j);
-            i++; j--;
-        }
+      perm(nums);
     }
 }
