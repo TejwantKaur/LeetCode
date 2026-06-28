c9bcb25ff919e5374f8465e8391e6504718b1471
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2026-06-28 14:26:06 +0530

    Time: 7 ms (99.1%), Space: 49 MB (73.29%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,28 +1,43 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        int n = intervals.length; // 4
-        if(n==0) return new int[0][0];
+  public List<int[]> mer(int[][] arr, int n){
+    Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+    List<int[]> list = new ArrayList<>();
 
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        List<int[]> ans = new ArrayList<>();
-      
-        int st = intervals[0][0];
-        int end = intervals[0][1];
+    int[] prev = arr[0];  // [1,3],[2,6]
 
-        for(int i=1; i<n; i++){
-            int[] curr = intervals[i];
+    for(int i=1; i<n; i++){
+      int[] curr = arr[i];
 
-            if(curr[0] > end){
-                ans.add(new int[] {st,end});
-                st = curr[0];
-                end = curr[1];
-            }
-            else {
-                // st = Math.min(st,curr[0]);
-                end = Math.max(end,curr[1]); 
-            }         
-        }
-        ans.add(new int[] {st,end});
-        return ans.toArray(new int[ans.size()][]);
+      if(prev[1] >= curr[0]){
+        // kida ending index vdda  [1,5], [2,4] --> 1,5
+        prev[1] = Math.max(prev[1], curr[1]); 
+      } else {
+        // prev[1] < curr[0]   // [2,6],[8,10]
+        list.add(prev);
+        prev = curr;
+      }
     }
+    list.add(prev);
+    return list;
+  }
+  public int[][] merge(int[][] intervals) {
+    int n = intervals.length;
+    List<int[]> list = mer(intervals, n);
+
+    return list.toArray(new int[list.size()][]);
+
+
+    
+    // int curr=intervals[0];
+    // for(int i=1; i<intervals.length; i++){
+    //   while(i<intervals.length && intervals[i-1][1] >= intervals[i][0]
+    //       && intervals[i-1][1] <= intervals[i][1])
+    //     i++;
+
+    //   list.add(new int[] {intervals[curr][0], intervals[i-1][1]});
+    //   curr = i; 
+    // }
+
+    
+  }
 }
